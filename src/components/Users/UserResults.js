19be159624fea8dc3244235dc65f4212b61c731f
ellenchen <chialin76@gmail.com>
2022-02-04 15:56:48 +0800

    init

---
+++ src/components/Users/UserResults.js
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+
+import Spinner from '../Layout/Spinner'
+import UserItem from './UserItem'
+import GithubContext from '../../context/github/GithubContext'
+
+const UserResults = () => {
+  const { users, loading } = useContext(GithubContext)
+
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  } else {
+    return <Spinner />
+  }
+}
+
+export default UserResults
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
+
+const GithubContext = createContext()
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
+
+  // takes a reducer function and an initial state as input and returns the current state and a dispatch function as output
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export default GithubContext
